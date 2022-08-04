976bcbe87686dabb94e4cd214af2897b07959f14
homero <homerogazze1015@gmail.com>
2022-08-04 17:36:39 -0300

    [NEW] apollo client created and connected

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:4000"
+  })
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 )
