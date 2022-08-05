7b611925466e72ba698b059b01a7a21e352a5b84
homero <homerogazze1015@gmail.com>
2022-08-05 16:15:06 -0300

    [NEW] Database Model Person Created

---
+++ graphql_server/models/Person.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseUniqueValidator from "mongoose-unique-validator";
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+        unique: true,
+        minlength: 5 
+    },
+    phone: {
+        type: String,
+        minlength: 5 
+    },
+    street: {
+        type: String,
+        require: true,
+        minlength: 5 
+    },
+    city: {
+        type: String,
+        require: true,
+        minlength: 5 
+    },
+})
+
+schema.plugin(mongooseUniqueValidator)
+export default mongoose.model('Person', schema)
